2ab90c87b170287088772c0b83d70924e56fb228
Mustapha Bouhsen <mustapha.bouhsen.1@ulaval.ca>
2021-09-26 12:13:51 -0400

    arret

--- aRemettrePourTP1.cpp
+++ aRemettrePourTP1.cpp
@@ -32,12 +32,14 @@ void DonneesGTFS::ajouterLignes(const std::string &p_nomFichier) {
         // Ajouter au m_ligne les cles et leurs valuers
         m_lignes.insert(pair<unsigned int, Ligne>(stoi(temp.at(0)),
                                                   Ligne(stoi(temp.at(0)),temp.at(2),
-                                                        temp.at(4),Ligne::couleurToCategorie(temp.at(7)))));
+                                                        temp.at(4),
+                                                        Ligne::couleurToCategorie(temp.at(7)))));
 
         // Ajouter au m_ligne les cles et leurs valuers
         m_lignes_par_numero.insert(pair<string , Ligne>(temp.at(2),
                                                         Ligne(stoi(temp.at(0)),temp.at(0),
-                                                              temp.at(4),Ligne::couleurToCategorie(temp.at(7)))));
+                                                              temp.at(4),
+                                                              Ligne::couleurToCategorie(temp.at(7)))));
 
         // supprimer les elements deja utliser.
         temp.clear();
@@ -165,9 +167,12 @@ void DonneesGTFS::ajouterVoyagesDeLaDate(const std::string &p_nomFichier)
                                                          stoi(temp.at(0)), temp.at(1),
                                                          temp.at(3))));
         }
+
+        // supprimer les elements deja utliser
         temp.clear();
     }
 
+    // Fermer le fichier ouvert
     ifs.close();
 }
 
@@ -180,9 +185,49 @@ void DonneesGTFS::ajouterVoyagesDeLaDate(const std::string &p_nomFichier)
 //! \throws logic_error si un problème survient avec la lecture du fichier
 void DonneesGTFS::ajouterArretsDesVoyagesDeLaDate(const std::string &p_nomFichier)
 {
+    ifstream ifs(p_nomFichier, ios::in);
+    if (!ifs) {
+        throw logic_error("Le fichier n'existe pas");
+    }
 
-//écrire votre code ici
+    string premiereLigne;
+    getline(ifs, premiereLigne);
+    string ligneFichier;
+    vector<string> temp;
 
+    while (getline(ifs, ligneFichier)) {
+
+        // Enlever les "" du string
+        ligneFichier.erase(remove(ligneFichier.begin(), ligneFichier.end(), '"'), ligneFichier.end());
+        temp = string_to_vector(ligneFichier, ',');
+
+        Heure debut = Heure(stoi(temp.at(1).substr(0,2)),
+                            stoi(temp.at(1).substr(3,2)),
+                            stoi(temp.at(1).substr(6,2)));
+
+        Heure fin = Heure(stoi(temp.at(2).substr(0,2)),
+                          stoi(temp.at(2).substr(3,2)),
+                          stoi(temp.at(2).substr(6,2)));
+
+        if(m_voyages.find(temp.at(0)) != m_voyages.end() & m_now1 <=  debut & m_now2 > fin)
+        {
+            Arret::Ptr a_ptr = make_shared<Arret>(stoi(temp.at(3)),debut, fin,
+                                                  stoi(temp.at(4)),temp.at(0));
+
+           m_voyages[temp.at(0)].ajouterArret(a_ptr);
+
+           m_nbArrets +=1;
+        }
+
+        // supprimer les elements deja utliser
+        temp.clear();
+    }
+
+
+
+
+    // Fermer le fichier ouvert
+    ifs.close();
 }
 
 
